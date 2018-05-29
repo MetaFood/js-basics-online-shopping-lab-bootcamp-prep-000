d94ea871a4775ff2b29ecd65523ce444a2d28a69
Matthew Smyth <msmyth1030@gmail.com>
2018-05-29 17:06:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var itemToRemove = searchCartForItemToRemove(itemName);
+  return itemToRemove ? removeItemFromCart(itemToRemove) : notifyUserThereIsNoItemToRemove();
 }
 
 function placeOrder(cardNumber) {
